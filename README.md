e562b36b0f5fb49492b398031017efa607e888d4
leopheard <leopheard.misc@gmail.com>
2021-10-11 16:30:22 -0400

    uploads

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-<a href="https://kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="">xxxxxxx</a> podcast.<br>
+<a href="https://kodi.tv">KODI<a> (XBMC/OSMC/Firestick) audio addon for the <a href="https://taibbi.substack.com/?utm_medium=podcast">TK To Go</a> podcast from Matt Taibbi's Substack feed.<br>
 
-<img src="https://shadowproof.com/wp-content/uploads/2015/07/sp-logo.png" width="250" height="250" alt="image text here"><br>
+<img src="https://github.com/leopheard/Audio-Podcasts/blob/master/resources/media/icon.jpg?raw=true" width="200" height="200" alt="Matt Taibbi TK To GO"><br>
 
--------- Podcast text here
+Audio versions of TK New's articles from the Substack of Matt Taibbi.<br>
 
 <a href="https://www.kodi.tv">Kodi</a> (formerly known as XBMC) is an award-winning free and open source (GPL) software media player and entertainment hub that can be installed on Linux, OSX, Windows, iOS and Android, featuring a 10-foot user interface for use with televisions and remote controls. It allows users to play and view most videos, music, podcasts, and other digital media files from local and network storage media and the internet.<br>
 
--- addon.py
+++ addon.py
@@ -2,18 +2,14 @@ from xbmcswift2 import Plugin, xbmcgui
 from resources.lib import mainaddon
 
 plugin = Plugin()
-url1 = ""
+url1 = "https://api.substack.com/feed/podcast/1042/private/c6d5ce08-4193-471e-902e-504b9b2e8336.rss"
 @plugin.route('/')
 def main_menu():
     items = [
         {
             'label': plugin.get_string(30001), 
             'path': plugin.url_for('episodes1'),
-            'thumbnail': ""},
-        {
-            'label': plugin.get_string(30002),
-            'path': plugin.url_for('episodes2'),
-            'thumbnail': ""},
+            'thumbnail': "https://github.com/leopheard/tktogo/blob/master/resources/media/icon.jpg?raw=true"},
     ]
     return items
 
@@ -22,13 +18,6 @@ def episodes1():
     soup1 = mainaddon.get_soup1(url1)
     playable_podcast1 = mainaddon.get_playable_podcast1(soup1)
     items = mainaddon.compile_playable_podcast1(playable_podcast1)
-    return items
-
-@plugin.route('/episodes2/')
-def episodes2():
-    soup1 = mainaddon.get_soup1(url1)
-    playable_podcast2 = mainaddon.get_playable_podcast2(soup1)
-    items = mainaddon.compile_playable_podcast2(playable_podcast2)
     return items
 
 if __name__ == '__main__':
